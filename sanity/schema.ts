58a26420f3888662d7078edcd2aa748071f81e69
HaaVad <123933986+HaaVad@users.noreply.github.com>
2024-02-29 22:46:13 +0100

    minor fix

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,7 +5,8 @@ import category from './schemas/category'
 import post from './schemas/post'
 import author from './schemas/author'
 import project from './schemas/project'
+import about from './schemas/about'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent, project],
+  types: [post, about, author, category, blockContent, project],
 }
--- sanity/schemas/about.ts
+++ sanity/schemas/about.ts
@@ -32,11 +32,6 @@ export default defineType({
       type: 'string',
     }),
     defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    }),
-    defineField({
       name: 'bio',
       title: 'Bio',
       type: 'array',
